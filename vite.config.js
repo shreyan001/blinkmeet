b9cfdfdc35f37c7323fde9d7629101871f3a7847
shreyan001 <shreyan010@gmail.com>
2023-02-10 19:51:52 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
      proxy:{
-      '/api' : 'http://localhost:3000' && 'https://spacemeetapi.vercel.app/'
+      '/api' : 'https://spacemeetapi.vercel.app/'
      }
   },
   plugins: [react()]
